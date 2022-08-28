9b5e282d36c0a989b50e55c1c8c32c056fa1753a
Tessa Grainger <tessa.robyn@outlook.com>
2022-08-28 16:35:59 +0100

    Basic model and lighting set up

--- laptop.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("laptopContainer");
-const engine = new BABYLON.Engine(canvas, true);
-
-BABYLON.SceneLoader.Load("obj/", "LaptopFull.glb", engine, function (scene) {
-  scene.createDefaultCameraOrLight(true, true, true);
-
-  engine.runRenderLoop(() => scene.render());
-});
