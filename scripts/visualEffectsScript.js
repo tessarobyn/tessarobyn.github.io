13240d8f5e9b83b21de04c5fcf8d7cd6bd448155
Tessa Grainger <tessa.robyn@outlook.com>
2025-06-01 03:23:16 +0100

    Removed debugging logs and obsolete code

--- scripts/visualEffectsScript.js
+++ scripts/visualEffectsScript.js
@@ -13,11 +13,9 @@ img.style.objectPosition = x + "px " + y + "px";
 const checkBackgroundRatio = () => {
   const container = document.getElementById("landingImgContainer");
   if (container.offsetWidth >= container.offsetHeight) {
-    console.log("changingWidth");
     img.style.height = "auto";
     img.style.width = "100%";
   } else {
-    console.log("changingHeight");
     img.style.width = "auto";
     img.style.height = "100%";
   }
@@ -40,7 +38,6 @@ const moveBackground = (newMouseX, newMouseY) => {
   const movementAllowance =
     maxMovementAllowance > smallerArea ? smallerArea : maxMovementAllowance;
 
-  console.log(movementAllowance);
   if (currentMouseX > newMouseX) {
     x = x >= -movementAllowance ? x - moveAmount : x;
   } else {
@@ -57,13 +54,7 @@ const moveBackground = (newMouseX, newMouseY) => {
 
   currentMouseX = newMouseX;
   currentMouseY = newMouseY;
-  //   const currentImgPos = img.style.objectPosition;
-  //   console.log(currentImgPos); -> should be stored in x and y
-
-  //   x boundary = smallerArea to containerWidth+smallerArea. Same principle for y
 };
-
-// alert(x);
 
 document.body.addEventListener("mousemove", (e) => {
   moveBackground(e.clientX, e.clientY);
@@ -73,18 +64,3 @@ window.addEventListener("resize", (e) => {
   console.log("here");
   checkBackgroundRatio();
 });
-
-// const height = container.clientHeight;
-// const width = container.clientWidth;
-// if (width >= height) {
-//   container.style.backgroundSize = width * 1.2 + "px";
-// }
-// container.style.backgroundPosition =
-//   -width * 0.1 + " px" + -height * 0.1 + "px";
-
-// console.log("done");
-// container.addEventListener("mousemove", (e) => {
-//   container.style.backgroundPositionX = e.clientX + "px";
-//   container.style.backgroundPositionY = e.clientY + "px";
-//   console.log(e.clientX, e.clientY);
-// });
